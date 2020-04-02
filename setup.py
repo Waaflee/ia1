4d063d710eff8f015a5ad740af820bf774c48b8b
Waaflee <agustinwafle@gmail.com>
2020-04-02 03:36:17 -0300

    Added multiprocessing to setup.py 2:23 -> 0:49

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import pandas as pd
 from typing import Iterable, List
 from utils.processing import extract_features
 import os
+import multiprocessing
 
 prefix: str = "dataset/"
 folders: Iterable[str] = ["nails", "screws", "washers", "nuts"]
@@ -11,11 +12,13 @@ def setup() -> None:
     dataset: List[Dict[str, Union[int, str, float]]] = []
     for folder in folders:
         images: List[str] = os.listdir(f"{prefix}{folder}")
-        for image in images:
+        with multiprocessing.Pool(processes=4) as pool:
             classification: str = folder[:-1]
-            data = extract_features(f"{prefix}{folder}/{image}")
-            data["classification"] = classification
-            dataset.append(data)
+            data = pool.map(extract_features, [
+                            f"{prefix}{folder}/{image}" for image in images])
+            for i in data:
+                i["classification"] = classification
+            dataset += data
     else:
         df = pd.DataFrame(dataset)
         print(df)
