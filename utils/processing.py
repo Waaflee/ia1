68042dab679fadc32276cdd6881bed921d6e5ea9
Waaflee <agustinwafle@gmail.com>
2020-04-02 20:17:10 -0300

    Implemented humoments feature extraction

--- utils/processing.py
+++ utils/processing.py
@@ -1,8 +1,10 @@
 import cv2
 import numpy as np
-from typing import Dict, Union
+from typing import Dict, Union, List
 
 from matplotlib import pyplot as plt
+from numpy.core.umath import log10
+from math import copysign
 
 
 def display(name: str, img) -> None:
@@ -14,7 +16,46 @@ def display(name: str, img) -> None:
 
 
 def to_hu_moments(filename: str) -> List[float]:
-    pass
+    im = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    # im = cv2.imread(filename)
+    # Preprocessing
+    # Grayscale
+    # gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+   # Binary Image
+    ret, thresh = cv2.threshold(im, 100, 255, 0)
+    # Processing
+    # Finding contours
+    contours, hierarchy = cv2.findContours(
+        thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    # Sorting Contours
+    # contours.sort(key=lambda x: len(x), reverse=True)
+    contours.sort(key=lambda x: cv2.contourArea(x), reverse=True)
+    cnt = contours[1]  # First is a box containing the hole image.
+
+    x, y, w, h = cv2.boundingRect(cnt)
+    im = im[y:y+h, x:x+w]
+
+    if debug:
+        cv2.imshow("binary", im)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    im = cv2.GaussianBlur(im, (5, 5), 0)
+    _, im = cv2.threshold(im, 95, 255, cv2.THRESH_BINARY)
+
+    if debug:
+        cv2.imshow("binary", im)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    # Calculate Moments
+    moments = cv2.moments(im)
+    # Calculate Hu Moments
+    huMoments = cv2.HuMoments(moments)
+    # Log scale hu moments
+    for i in range(0, 7):
+        huMoments[i] = -1 * \
+            copysign(1.0, huMoments[i]) * log10(abs(huMoments[i]))
+    return huMoments
 
 
 def to_ar_and_corners(filename: str) -> Dict[str, Union[int, str, float]]:
@@ -110,33 +151,19 @@ def to_ar_and_corners(filename: str) -> Dict[str, Union[int, str, float]]:
     }
 
 
-debug = False
-
-
 def main() -> None:
     print("-"*25)
     print("Processing test: ")
     test = ["dataset/nails/nail", "dataset/screws/screw",
             "dataset/washers/washer", "dataset/nuts/nut"]
     postfix = ["_1.jpg", "_2.jpg", "_3.jpg"]
-    # postfix = ["_1.jpg"]
-    pictures = []
+    postfix = ["_1.jpg"]
     data = []
     for i in test:
         for j in postfix:
-            # if debug:
-            #     d, img = extract_features(f"{i}{j}")
-            #     pictures.append(img)
-            # else:
-            d = extract_features(f"{i}{j}")
+            d = to_hu_moments(f"{i}{j}")
             data.append(d)
-    for i in data:
-        print(i)
-    # if debug:
-    #     image = cv2.hconcat(pictures)
-    #     display("Processed set", image)
-    #     cv2.waitKey(0)
-    #     cv2.destroyAllWindows()
+            print(d)
 
 
 if __name__ == "__main__":
