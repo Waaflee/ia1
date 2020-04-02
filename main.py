62d0ca7d222d13343068ec257c9ed8dec7e06825
Waaflee <agustinwafle@gmail.com>
2020-04-02 03:20:23 -0300

    Added data visualization

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-import cv2
-import numpy as np
-import os
-from utils.processing import extract_features
+import pandas as pd
+from matplotlib import pyplot as plt
+
+data = "data/dataset.csv"
 
 
-print(extract_features("nail_1.jpg"))
+def main() -> None:
+    df = pd.read_csv(data, index_col=0)
+    dataset: List[Dict] = df.to_dict('records')
+
+    # Plot the data
+    plt.figure(figsize=(6, 6))
+    plt.scatter([i["aspect_ratio"] for i in dataset],
+                [i["corners"] for i in dataset])
+    plt.xlabel('Aspect Ratio')
+    plt.ylabel('Amount of Corners')
+    plt.title('Visualization of Extracted Features')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
